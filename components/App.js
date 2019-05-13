ebed13d42b2a56bab4dc6bbbae9d78d16eec4c11
Ben Parisot <me@benparisot.com>
2019-05-13 16:23:57 -0700

    get dog component displaying

---
+++ components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Dog from './Dog';
+
+export default function App() {
+  return (
+        <>
+            {/* <Header /> */}
+            <Dog name="Joe" age={5} weight="90 lbs" />
+        </>
+  );
+}
