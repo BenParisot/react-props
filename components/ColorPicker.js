84c3fcb1215a390f0f4e6380a7c12e3cbaff9863
Ben Parisot <me@benparisot.com>
2019-05-14 09:55:27 -0700

    disable eslint on event line in colorpicker component

--- components/ColorPicker.js
+++ components/ColorPicker.js
@@ -2,15 +2,15 @@ import React, { PureComponent } from 'react';
 // import PropTypes from 'prop-types';
 
 export default class ColorPicker extends PureComponent {
-
+//eslint-disable-next-line
     redHandler = event => {
       console.log('red');
     }
-
+    //eslint-disable-next-line
     blueHandler = event => {
       console.log('blue');
     }
-
+    //eslint-disable-next-line
     yellowHandler = event => {
       console.log('yellow');
     }
