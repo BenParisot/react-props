5f6c7ed58510fbb63da0ee234595fd04a3ce65a4
Ben Parisot <me@benparisot.com>
2019-05-13 20:26:03 -0700

    color console log works

--- components/ColorPicker.js
+++ components/ColorPicker.js
@@ -1,15 +1,28 @@
 import React, { PureComponent } from 'react';
+// import PropTypes from 'prop-types';
 
 export default class ColorPicker extends PureComponent {
-  render() {
-    return (
+
+    redHandler = event => {
+      console.log('red');
+    }
+
+    blueHandler = event => {
+      console.log('blue');
+    }
+
+    yellowHandler = event => {
+      console.log('yellow');
+    }
+    render() {
+      return (
         <>
-        <button style="background-color:red;">Red</button>
-        <button style="background-color:blue;">Blue</button>
-        <button style="background-color:yellow;">Yellow</button>
+        <button onClick={this.redHandler} style={{ background:'red' }}>Red</button>
+        <button onClick={this.blueHandler} style={{ background:'blue' }}>Blue</button>
+        <button onClick={this.yellowHandler} style={{ background:'yellow' }}>Yellow</button>
       </>
 
-    );
-  }
+      );
+    }
 
 }
